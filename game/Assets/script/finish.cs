3be47d0118479c0e886b87a159d4ec150510badd
RANIAKHALE <you@RANIAKHALE.com>
2022-05-18 14:42:13 +0200

    my frist game

---
+++ game/Assets/script/finish.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class finish : MonoBehaviour
+{
+    private bool levelCompleted = false;
+    private AudioSource finishSound;
+    void Start()
+    {
+        finishSound = GetComponent<AudioSource>();
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "player" && !levelCompleted)
+        {
+            finishSound.Play();
+            levelCompleted = true;
+            Invoke("CompleteLevel", 2f);
+        }
+    }
+    private void CompleteLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
